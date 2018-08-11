0e013577d5a8f0141a94df2b6301084929c5c7be
LuK1337 <priv.luk@gmail.com>
2018-08-11 16:27:29 +0200

    zl1: Switch to new NXP NFC
    Change-Id: Idbf8506c5e7c77b56d131bcd857c29a643f46685

--- device.mk
+++ device.mk
@@ -70,11 +70,10 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service \
+    android.hardware.nfc@1.1-service \
     com.android.nfc_extras \
-    nfc_nci.msm8996 \
     NfcNci \
+    SecureElement \
     Tag
 
 # Fake logprint for fingerprint libs
